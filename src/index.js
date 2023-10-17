8b6213116db9f58c4d32ca34b5bc2559c84d1bc2
Jacque <Jacque@DESKTOP-VNPE1N4>
2023-10-17 13:14:56 +0300

    modify

--- src/index.js
+++ src/index.js
@@ -6,19 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import store from './app/store'
 import { Provider } from 'react-redux'
 import SuspenseContent from './containers/SuspenseContent';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <GoogleOAuthProvider clientId="784871870828-j3t3cua55c74uhiqv4bbh7rfch7mfrak.apps.googleusercontent.com">
+  
     <Suspense fallback={<SuspenseContent />}>
         <Provider store={store}>
             <App />
         </Provider>
     </Suspense>
-    </GoogleOAuthProvider>,
-  // </React.StrictMode>
+   
 );
 
 // If you want to start measuring performance in your app, pass a function
